555019cb02175dfa3022e0409d30af84e8d58606
varinder <nmvarinder@gmail.com>
2025-01-01 15:26:03 +0530

    error: resolving

---
+++ src/lib/components/settings/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import {goto} from '$app/navigation'
+    export let button
+</script>
+
+<button class="button" role="button" on:click={goto(button.link)}>
+    <div class="logo"></div>
+    <div class="text">{button.text}</div>
+</button>
---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import Button from '$lib/components/settings/Button.svelte'
+
+    const buttons = [
+        {logo: '', text: 'general', link: 'settings/general'},
+        {logo: '', text: 'theme', link: 'settings/theme'},
+        {logo: '', text: 'username', link: 'settings/username'},
+        {logo: '', text: 'import', link: 'settings/import'},
+        {logo: '', text: 'export', link: 'settings/general'},
+        {logo: '', text: 'logout', link: 'settings/logout'}
+
+    ]
+</script>
+
+<div class="h-full p-4 flex flex-col justify-center">
+    {#each buttons as item}
+        <Button button={item} />
+    {/each}
+</div>
